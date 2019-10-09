aab7569ff62136e7c3753930e5e42f3539cfa96f
tnazikian <txn81@case.edu>
2019-10-08 20:07:36 -0700

    updated notebook

--- helper.py
+++ helper.py
@@ -3,3 +3,7 @@ import numpy as np
 def sigmoid_gyaku(x):
     """Takes in a fitness score and returns a value between 1 and 0"""
     return 2/(1+np.exp(x))
+
+def add_noise(y, mu, std):
+    noise = np.random.normal(mu, std, y.shape)
+    return y + noise
